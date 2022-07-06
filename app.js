09c6fc505b81458259df9040f837ef217f570c3c
Alex Tracey <alex.tracey@Alex-Traceys-MacBook-Pro.local>
2022-07-06 10:41:24 +0100

    GET reviews by order of date function with error handling

--- app.js
+++ app.js
@@ -12,15 +12,14 @@ const {
 
 
 const { getAllCategories } = require("./controllers/categories_controller.js")
-const {getReviewByID, patchReviewById} = require("./controllers/reviews_controller.js")
+const {getReviewByID, patchReviewById, getAllReviews} = require("./controllers/reviews_controller.js")
 const { getAllUsers } = require("./controllers/users_controller.js")
-
 
 app.get("/api/categories", getAllCategories);
 app.get("/api/reviews/:review_id", getReviewByID)
 app.patch("/api/reviews/:review_id", patchReviewById);
 app.get("/api/users", getAllUsers);
-
+app.get("/api/reviews", getAllReviews);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "Not Found" });
